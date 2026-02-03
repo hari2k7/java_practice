6c9d05cd9987b7a2209c2c4f52616bbed84bb479
hari2k7 <hari082007@gmail.com>
2026-02-03 23:20:38 +0530

    Update AsteriskPyramid.java

--- src/com/hari/java/practice_exercises/patterns/AsteriskPyramid.java
+++ src/com/hari/java/practice_exercises/patterns/AsteriskPyramid.java
@@ -3,8 +3,20 @@ package com.hari.java.practice_exercises.patterns;
 import java.util.Scanner;
 
 public class AsteriskPyramid {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int r = scanner.nextInt();
+
+        for(int i=1; i<=r; i++){
+            for(int j=0; j<r-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=0; k<i; k++){
+                System.out.print("*"+" ");
+            }System.out.println();
+        }
+
 
         scanner.close();
     }
