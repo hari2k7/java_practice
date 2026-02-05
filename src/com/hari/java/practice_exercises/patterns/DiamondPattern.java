502be93ede92e5c27d5b0b1fdb69038c5492d0e8
hari2k7 <hari082007@gmail.com>
2026-02-05 17:34:37 +0530

    Add file to patterns

---
+++ src/com/hari/java/practice_exercises/patterns/DiamondPattern.java
@@ -0,0 +1,28 @@
+package com.hari.java.practice_exercises.patterns;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
