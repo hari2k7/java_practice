be77f2ff3d660c2d49d6b20d7ba0acd1d0abad93
hari2k7 <hari082007@gmail.com>
2026-02-24 12:07:55 +0530

    Add files to patterns

---
+++ src/com/hari/java/practice_exercises/patterns/PascalDiamondPattern.java
@@ -0,0 +1,42 @@
+package com.hari.java.practice_exercises.patterns;
+
+import java.util.Scanner;
+
+/*
+ * Program to print Pascal's Triangle in Diamond Form
+ */
+public class PascalDiamondPattern {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            int num = 1;
+            for (int space = 0; space < n - i - 1; space++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + "   ");
+                num = num * (i - j) / (j + 1);
+            }System.out.println();
+        }
+        for (int i = n - 2; i >= 0; i--) {
+
+            int num = 1;
+
+            for (int space = 0; space < n - i - 1; space++) {
+                System.out.print("  ");
+            }
+
+            for (int j = 0; j <= i; j++) {
+                System.out.print(num + "   ");
+                num = num * (i - j) / (j + 1);
+            }System.out.println();
+        }
+
+        scanner.close();
+    }
+}
